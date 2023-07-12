0440279b6021a300e5f0f023838dcfcd5d31d492
Pavel Holy <gyro1125@gmail.com>
2023-07-12 18:33:30 +0200

    Fix monitor positioning, set high refresh rate and fix scaling issue in chrome (dpi)

--- configuration.nix
+++ configuration.nix
@@ -240,6 +240,7 @@ in
         modifier = "Mod4";
         terminal = "alacritty";
       };
+      extraConfig = ''exec --no-startup-id xrandr --dpi 96 --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 5120x0 --rotate normal --rate 165 --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate normal --rate 165 --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --rate 165 --output DP-5 --off'';
     };
 
     # ----- SETTINGS SWAY -----
