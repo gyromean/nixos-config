b9899b2edffdf6e4fdf5c9d6bde834778bab7a18
Pavel Holy <gyro1125@gmail.com>
2023-08-17 14:40:00 +0200

    Nvim: Unbind <C-p> from cmp, was colliding with my custom {} insert bind

--- configuration.nix
+++ configuration.nix
@@ -595,6 +595,7 @@ local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
 cmp.setup({
   mapping = {
     ['<CR>'] = cmp.config.disable,
+    ['<C-P>'] = cmp.config.disable, -- for some reason to musi byt velky P
     ['<C-d>'] = cmp.mapping(function(fallback) fallback() end, {"s"}), -- disablovat <C-d> v select modu, idk jestli je to dobre
     ['<Tab>'] = cmp.mapping(function(fallback) fallback() end, {"s", "i"}), -- disablovat <C-d> v select modu, idk jestli je to dobre
 
