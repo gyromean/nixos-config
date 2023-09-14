a621504ea2eded0a31ff4503a27d908eca9421f2
Pavel Holy <gyro1125@gmail.com>
2023-09-14 19:36:26 +0200

    Add laptop specific vars and settings files

---
+++ laptop/nix_files/vars.nix
@@ -0,0 +1,5 @@
+{
+  hostname = "pavellt";
+  monitorSetup = "xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off";
+  polybarBrightness = '''';
+}
