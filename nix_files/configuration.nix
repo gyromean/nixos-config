9ba5b94981395fedee4c73ef0d931cd2aaf7767d
Pavel Holy <gyro1125@gmail.com>
2023-09-14 18:34:59 +0200

    Fix (almost) screen tearing when switching workspaces

--- nix_files/configuration.nix
+++ nix_files/configuration.nix
@@ -416,6 +416,11 @@ done
     # ----- SETTINGS PICOM -----
     services.picom = {
       enable = true; # bez picomu je za polybarem cerna cast kdyz nema width 100%
+      vSync = true; # opravit screen tearing, viz https://unix.stackexchange.com/questions/421622/how-to-get-rid-of-background-flickering-when-switching-workspaces
+      backend = "glx";
+      fade = true;
+      fadeDelta = 1;
+      fadeSteps = [ 1 1 ];
     };
 
     # ----- SETTINGS FZF ------
