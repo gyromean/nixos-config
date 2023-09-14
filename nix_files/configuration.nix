c487a46bb48824abfbe32fe966360fa4ac92d717
Pavel Holy <gyro1125@gmail.com>
2023-09-14 16:09:12 +0200

    Move monitor setup to desktop_vars.nix

--- nix_files/configuration.nix
+++ nix_files/configuration.nix
@@ -269,7 +269,7 @@ in
         modifier = mod;
         terminal = "xfce4-terminal";
         startup = [
-          { command = "xrandr --dpi 96 --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --rate 165 --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 5120x0 --rotate normal --rate 165 --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 2560x0 --rotate normal --rate 165 --output DP-5 --off"; notification = false; } # nastavi monitory na spravny poradi a spravny refresh rate; `--dpi 96` nastavi scaling UI elementu, ruzny aplikace na to berou ohled (treba chrome)
+          { command = machine.monitorSetup; notification = false; } # nastavi monitory na spravny poradi a spravny refresh rate; `--dpi 96` nastavi scaling UI elementu, ruzny aplikace na to berou ohled (treba chrome)
           { command = "feh --bg-fill ~/.config/nixos-config/wallpaper.png"; notification = false; } # nastaveni wallapper na startupu
           { command = "xset r rate 175 30"; notification = false; } # nastaveni prodlevy pred key repeatem na 175 ms, frekvence key repeatu na 30 Hz
           { command = "numlockx on"; notification = false; } # zapnout numlock pri bootu
