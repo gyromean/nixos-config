13ed90e65bb6f50f53261c6c77cc3e2492eb4641
Pavel Holy <gyro1125@gmail.com>
2023-09-16 10:43:43 +0200

    Add missing spaces to polybar script brightness-laptop.py

--- common/polybar_scripts/brightness-laptop.py
+++ common/polybar_scripts/brightness-laptop.py
@@ -15,7 +15,7 @@ def set_output_raw(data):
 
 def set_output(value):
   icon_index = value // 5
-  data = set_color('blue', icons_distributed[icon_index]) + set_color('foreground', f' {value}%')
+  data = ' ' + set_color('blue', icons_distributed[icon_index]) + set_color('foreground', f' {value}% ')
   return set_output_raw(data)
 
 async def socket_reader_client(reader, writer):
