904d313faca4b27be3bc4a4647b64e1cb1102f72
Pavel Holy <gyro1125@gmail.com>
2024-02-22 10:25:17 +0100

    Nvim: Tweak dapui settings

--- common/dotfiles/nvim/plugin-config/dap.lua
+++ common/dotfiles/nvim/plugin-config/dap.lua
@@ -24,6 +24,77 @@ dap.configurations.c = {
   },
 }
 
-require("dapui").setup()
+require("dapui").setup({
+    controls = {
+      element = "repl",
+      enabled = true,
+      icons = {
+        disconnect = "",
+        pause = "",
+        play = "",
+        run_last = "",
+        step_back = "",
+        step_into = "",
+        step_out = "",
+        step_over = "",
+        terminate = ""
+      }
+    },
+    element_mappings = {},
+    expand_lines = true,
+    floating = {
+      border = "single",
+      mappings = {
+        close = { "q", "<Esc>" }
+      }
+    },
+    force_buffers = true,
+    icons = {
+      collapsed = "",
+      current_frame = "",
+      expanded = ""
+    },
+    layouts = { {
+        elements = { {
+            id = "scopes",
+            size = 0.55
+          }, {
+            id = "breakpoints",
+            size = 0.15
+          }, {
+            id = "stacks",
+            size = 0.15
+          }, {
+            id = "watches",
+            size = 0.15
+          } },
+        position = "left",
+        -- size = 40
+        size = 70
+      }, {
+        elements = { {
+            id = "repl",
+            size = 0.7
+          }, {
+            id = "console",
+            size = 0.3
+          } },
+        position = "bottom",
+        size = 10
+      } },
+    mappings = {
+      edit = "e",
+      expand = { "<CR>", "<2-LeftMouse>" },
+      open = "o",
+      remove = "x",
+      repl = "r",
+      toggle = "t"
+    },
+    render = {
+      indent = 1,
+      max_value_lines = 100
+    }
+  }
+)
 
 require("nvim-dap-virtual-text").setup()
