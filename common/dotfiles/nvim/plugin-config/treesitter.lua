dbfb2f48c44dfdca79720dc30dc074335f8d9a7b
Pavel Holy <gyro1125@gmail.com>
2024-02-22 10:14:41 +0100

    Nvim: Fix indentation bug in python files

--- common/dotfiles/nvim/plugin-config/treesitter.lua
+++ common/dotfiles/nvim/plugin-config/treesitter.lua
@@ -1,5 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = { "python" }, -- fix pro rozbity indentation kdyz je { treba ve stringu nebo komentari, viz https://github.com/nvim-treesitter/nvim-treesitter/issues/1573#issuecomment-1780727057
   },
 }
