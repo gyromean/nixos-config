57cce1a1e64d609e712c77d133d0a8d385cfea3c
Pavel Holy <gyro1125@gmail.com>
2024-02-28 13:37:01 +0100

    Migrate to alacritty from wezterm, because wezterm is laggy (upgrading to nixpkgs-unstable seems to fix that issue though)

--- common/nix-files/configuration.nix
+++ common/nix-files/configuration.nix
@@ -352,7 +352,7 @@ in
       enable = true;
       config = rec {
         modifier = "Mod4";
-        terminal = "wezterm";
+        terminal = "alacritty";
         startup = [
           { command = machine.monitorSetup; notification = false; } # nastavi monitory na spravny poradi a spravny refresh rate; `--dpi 96` nastavi scaling UI elementu, ruzny aplikace na to berou ohled (treba chrome)
           { command = "feh --bg-fill ~/.config/nixos-config/wallpaper.png"; notification = false; } # nastaveni wallapper na startupu
@@ -718,74 +718,7 @@ source /home/pavel/.config/nixos-config/common/dotfiles/nvim/init.vim
     programs.alacritty = {
       enable = true;
     };
-    # manualne nastavit konfigurak, protoze pres home-manager to replacuje newlines doslovnymi '\n' (reverse engineernuto z https://github.com/nix-community/home-manager/blob/master/modules/programs/alacritty.nix)
-    # colorscheme z https://github.com/nordtheme/alacritty/blob/main/src/nord.yaml
-    # nastaveni fontu z https://wiki.archlinux.org/title/Alacritty#Font
-    xdg.configFile."alacritty/alacritty.yml".text = ''
-colors:
-  primary:
-    background: "#2e3440"
-    foreground: "#d8dee9"
-    dim_foreground: "#a5abb6"
-  cursor:
-    text: "#2e3440"
-    cursor: "#d8dee9"
-  vi_mode_cursor:
-    text: "#2e3440"
-    cursor: "#d8dee9"
-  selection:
-    text: CellForeground
-    background: "#4c566a"
-  search:
-    matches:
-      foreground: CellBackground
-      background: "#88c0d0"
-    footer_bar:
-      background: "#434c5e"
-      foreground: "#d8dee9"
-  normal:
-    black: "#3b4252"
-    red: "#bf616a"
-    green: "#a3be8c"
-    yellow: "#ebcb8b"
-    blue: "#81a1c1"
-    magenta: "#b48ead"
-    cyan: "#88c0d0"
-    white: "#e5e9f0"
-  bright:
-    black: "#4c566a"
-    red: "#bf616a"
-    green: "#a3be8c"
-    yellow: "#ebcb8b"
-    blue: "#81a1c1"
-    magenta: "#b48ead"
-    cyan: "#8fbcbb"
-    white: "#eceff4"
-  dim:
-    black: "#373e4d"
-    red: "#94545d"
-    green: "#809575"
-    yellow: "#b29e75"
-    blue: "#68809a"
-    magenta: "#8c738c"
-    cyan: "#6d96a5"
-    white: "#aeb3bb"
-
-font:
-  normal:
-    family: monospace
-    style: Regular
-  bold:
-    family: monospace
-    style: Bold
-  italic:
-    family: monospace
-    style: Italic
-  bold_italic:
-    family: monospace
-    style: Bold Italic
-  size: 11
-      '';
+    xdg.configFile."alacritty/alacritty.toml".source = config.lib.file.mkOutOfStoreSymlink "/home/pavel/.config/nixos-config/common/dotfiles/alacritty/alacritty.toml";
 
     # ----- SETTINGS TMUX -----
     programs.tmux = {
