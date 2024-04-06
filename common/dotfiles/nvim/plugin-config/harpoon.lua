0f1a25884f7dee67a5bed3fa5e44153303c1237a
Pavel Holy <gyro1125@gmail.com>
2024-04-06 12:48:23 +0200

    Nvim: Add binds for harpoon's 5th and 6th buffer

--- common/dotfiles/nvim/plugin-config/harpoon.lua
+++ common/dotfiles/nvim/plugin-config/harpoon.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, {desc 
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, {desc = "Harpoon select [2]. buffer"})
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, {desc = "Harpoon select [3]. buffer"})
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, {desc = "Harpoon select [4]. buffer"})
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, {desc = "Harpoon select [5]. buffer"})
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end, {desc = "Harpoon select [6]. buffer"})
