3c70ed44c32b630be1b6e5c8fceaedeb3af5b369
Pavel Holy <gyro1125@gmail.com>
2024-06-29 17:33:44 +0200

    Polybar: Add comments to and tweak audio.py

--- common/polybar-scripts/audio.py
+++ common/polybar-scripts/audio.py
@@ -1,5 +1,46 @@
 #!/usr/bin/env python
 
+# veci pro audio:
+# TODO: misto pactl pouzivat neco pro pipewire (ted tam jeste bezi nejakej pipewire-pulse, mozna to dela preklad or something)
+# TODO: asi ten sink mutnout jeste nez se prepnu na ten druhej
+# TODO: jeste zkopirovat ty TODO z headphones.sh
+# TODO: nekdy ten `bluetoothctl connect 00:C5:85:68:86:3E` timeoutne, tak v tom pripade ho asi pustit vickrat (retcode bude nenulovej)
+# TODO: ty pactl ids (to cislo kdyz dam pactl list short sinks si vzdycky nechat vypsat kdyz budu davat mute (a nebo mozna to delat rovnout pres to jmeno (alsa_output.pci-0000_0b_00.4.analog-stereo a bluez_output.00_C5_85_68_86_3E.1)))
+# TODO: kdyz mam pripojeny airpody a modifikuju hlasitost z nich ta se mi to nepropisuje do polybaru protoze to samozrejme nezapisuje do toho socketu, takze to by chtelo nejak prebindovat
+# BUG: na laptopu kdyz primo dam sluchatka do jacku, tak se mi treba unmutne audio, ale tenhle polybar modul to neukaze (takze asi potrebuju nejak odposlechnout event ze se zmenil sink nebo mute nebo tak neco (az se mi updatne verze wireplumberu, nemohl bych primo z ty lua sem nejak poslat ping (treba pres ten socket?)))
+# NOTE: prikazy: `bluetoothctl connect 00:C5:85:68:86:3E` na pripojeni, `pactl set-default-sink bluez_output.00_C5_85_68_86_3E.1` na prepnuti default sinku
+
+# ikony
+'''
+󰕿 󰖀 󰕾  - SPEAKER_ACTIVE
+󰸈      - SPEAKER_MUTED / SPEAKER_CONFIRM
+󰋋 󰟎    -
+󱡏 󱡐    -
+
+
+󰸴
+
+..|.
+'''
+
+# keywordy na nerdfonts
+'''
+speaker
+volume
+audio
+headphones <-- normalni sluchatka (overheady)
+earbuds <-- dost podobny airpodu
+'''
+
+# programy
+'''
+pactl
+pavucontrol
+
+pw-cli # pro dumpovani v JSONu pouzit pw-dump
+qpwgraph
+'''
+
 import asyncio, subprocess
 
 from polybaripc import PolybarIPC
@@ -51,10 +92,11 @@ class Impl(PolybarIPC):
     self.headphones_mac = headphones_mac
     self.volume = get_volume() // 5 * 5
     self.mute = get_mute()
-    self.bt_connected = self.is_bluetooth_connected() # NOTE: for future use
+    # self.bt_connected = self.is_bluetooth_connected() # NOTE: for future use
+    self.bt_connected = False
 
   def is_bluetooth_connected(self):
-    comp_proc = subprocess.run(['bluetoothctl', 'info', self.headphones_mac], capture_output=True)
+    comp_proc = subprocess.run(['bluetoothctl', 'info', self.headphones_mac], capture_output=True) # BUG: this call does not finish, happens after switching to a new nix channel (maybe it is because bluetooth is not running in the first place or something like that)
     output = comp_proc.stdout.decode()
     connected_lines = list(filter(lambda line: line.strip().startswith('Connected'), output.splitlines()))
     is_connected = len(connected_lines) and connected_lines[0].endswith('yes')
