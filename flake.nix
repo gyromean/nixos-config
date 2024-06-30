bdb19b4a431ecf50e482596f30bdc2b31b12f570
Pavel Holy <gyro1125@gmail.com>
2024-06-30 19:45:12 +0200

    Flake: Add function for generating configurations

--- flake.nix
+++ flake.nix
@@ -18,24 +18,33 @@
       configPath = "${homeDirectory}/.config/nixos-config";
     };
     lib = nixpkgs.lib;
+    mkNixosConfigs = hostsDirectories:
+    (builtins.listToAttrs
+      (builtins.map
+        (host:
+        let
+          machine = import ./hosts/${host}/vars.nix;
+        in {
+          name = machine.hostname;
+          value = lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit machine opts; };
+            modules = [
+              ./hosts/${host}
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users."${opts.username}" = import ./home;
+                home-manager.extraSpecialArgs = { inherit machine opts; };
+              }
+            ];
+          };
+        })
+        hostsDirectories
+      )
+    );
   in {
-    nixosConfigurations = {
-      pavelpc = let
-        machine = import ./hosts/desktop/vars.nix;
-      in lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit machine opts; };
-        modules = [
-          ./hosts/desktop
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pavel = import ./home;
-            home-manager.extraSpecialArgs = { inherit machine opts; };
-          }
-        ];
-      };
-    };
+    nixosConfigurations = mkNixosConfigs [ "desktop" ];
   };
 }
