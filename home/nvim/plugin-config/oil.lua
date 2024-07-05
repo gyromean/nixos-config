3b52fbe15825c9377c8c5350c4885233344c4cce
Pavel Holy <gyro1125@gmail.com>
2024-07-05 15:47:33 +0200

    Nvim: Tweak Oil keybinds

--- home/nvim/plugin-config/oil.lua
+++ home/nvim/plugin-config/oil.lua
@@ -1,1 +1,11 @@
-require'oil'.setup()
+require'oil'.setup({
+  keymaps = {
+    ["<CR>"] = "actions.select",
+    ["<BS>"] = "actions.parent",
+    ["gs"] = "actions.change_sort",
+    ["g."] = "actions.toggle_hidden",
+  },
+  use_default_keymaps = false,
+})
+
+vim.keymap.set({"n"}, "<BS>", "<cmd>Oil<cr>")
