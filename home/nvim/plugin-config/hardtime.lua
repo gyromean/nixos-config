2f79eb7b3c684c3fd1edf603161e4346644762b6
Pavel Holy <gyro1125@gmail.com>
2024-08-05 22:38:46 +0200

    Nvim: Disable unnecessary bullying from hardtime

--- home/nvim/plugin-config/hardtime.lua
+++ home/nvim/plugin-config/hardtime.lua
@@ -1,1 +1,22 @@
-require'hardtime'.setup()
+require'hardtime'.setup{
+  restricted_keys = {
+    ["h"] = {},
+    ["j"] = {},
+    ["k"] = {},
+    ["l"] = {},
+    ["-"] = {},
+    ["+"] = {},
+    ["gj"] = {},
+    ["gk"] = {},
+    ["<CR>"] = {},
+    ["<C-M>"] = {},
+    ["<C-N>"] = {},
+    ["<C-P>"] = {},
+  },
+  disabled_keys = {
+    ["<Up>"] = {},
+    ["<Down>"] = {},
+    ["<Left>"] = {},
+    ["<Right>"] = {},
+  },
+}
