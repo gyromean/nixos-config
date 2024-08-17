b4795956b570cfe8d5aa6c1286f2ad9e910025d1
Pavel Holy <gyro1125@gmail.com>
2024-08-17 19:59:54 +0200

    Nvim: Leap disable autojumping

--- home/nvim/plugin-config/leap.lua
+++ home/nvim/plugin-config/leap.lua
@@ -1,2 +1,6 @@
+local leap = require'leap'
+
 vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
 vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+
+leap.opts.safe_labels = {}
