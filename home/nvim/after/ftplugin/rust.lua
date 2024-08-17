f6d7f7c12aa45cc712302663b4ba76231c758401
Pavel Holy <gyro1125@gmail.com>
2024-08-17 21:05:06 +0200

    Nvim: Add options and debug print macro for rust

---
+++ home/nvim/after/ftplugin/rust.lua
@@ -0,0 +1,8 @@
+vim.opt.formatoptions:remove({"c", "r", "o"})
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.keymap.set({"n", "x"}, "<leader>p", [[:s/\v^( *)(.*)/\1eprintln!("\2 = {:?}", \2);<CR>:noh<CR>]], { buffer = true } );
