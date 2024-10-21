cbe6c6219991b1ee4821eab3a5380f8afe0e4c0c
Pavel Holy <gyro1125@gmail.com>
2024-10-21 09:11:06 +0200

    Nvim: Add equivalence classes for leap plugin for czech language

--- home/nvim/plugin-config/leap.lua
+++ home/nvim/plugin-config/leap.lua
@@ -4,3 +4,6 @@ vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
 vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
 
 leap.opts.safe_labels = {}
+leap.opts.equivalence_classes = {
+  'aá', 'cč', 'dď', 'eéě', 'ií', 'nň', 'oó', 'rř', 'sš', 'tť', 'uúů', 'yý', 'zž'
+}
