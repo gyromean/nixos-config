7935f7b1c7e2b6c7b7c835b67d37c3afd238176c
Pavel Holy <gyro1125@gmail.com>
2025-03-13 07:57:43 +0100

    VSCodium: Remove nord theme

--- home/vscode.nix
+++ home/vscode.nix
@@ -5,13 +5,11 @@
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       asvetliakov.vscode-neovim
-      arcticicestudio.nord-visual-studio-code
       ms-python.python
       ms-toolsai.jupyter
       ms-toolsai.jupyter-renderers
     ];
     userSettings = {
-      "workbench.colorTheme" = "Nord";
       "jupyter.widgetScriptSources" = ["jsdelivr.com" "unpkg.com"];
       "git.openRepositoryInParentFolders" = "never";
       "keyboard.dispatch" = "keyCode";
