345c4f832f09513a65f42d920b3cb50739fb1e19
Pavel Holy <gyro1125@gmail.com>
2025-07-18 14:57:08 +0200

    Bump flake

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -66,7 +66,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.gdm.enable = true;
   #services.xserver.desktopManager.gnome.enable = true; # TODO - tady jsem to zakomentaroval
 
   # Configure keymap in X11
