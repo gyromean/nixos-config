e8a86ac17e7bd5fe1424d9e9d403705e5f22ed47
Pavel Holy <gyro1125@gmail.com>
2025-07-19 17:45:53 +0200

    Nvim: Add bind to telescope live grep for selected text

--- home/nvim/plugin-config/telescope.lua
+++ home/nvim/plugin-config/telescope.lua
@@ -25,3 +25,38 @@ require'telescope'.setup{
 }
 require'telescope'.load_extension('fzf') -- kvuli extensionu, musi se to volat az po volani require'telescope'.setup, https://github.com/nvim-telescope/telescope-fzf-native.nvim
 require'telescope'.load_extension('ui-select') -- prettier code actions
+
+vim.keymap.set('x', '<leader>tg', function()
+  local start_pos = vim.fn.getpos('v')
+  local end_pos = vim.fn.getpos('.')
+
+  local buf = start_pos[1]
+
+  local start_row = start_pos[2] - 1
+  local start_col = start_pos[3]
+  local end_row = end_pos[2] - 1
+  local end_col = end_pos[3]
+
+  if start_row ~= end_row then
+    vim.print('Selection must not span multiple lines')
+    return
+  end
+
+  if start_col > end_col then
+    local tmp = start_col
+    start_col = end_col
+    end_col = tmp
+  end
+  start_col = start_col - 1
+
+  local selected_text = vim.api.nvim_buf_get_text(
+    buf,
+    start_row,
+    start_col,
+    end_row,
+    end_col,
+    {}
+  )[1]
+
+  require'telescope.builtin'.live_grep({ default_text = selected_text })
+end, { desc = 'Grep current selection' })
