9536eca98ef87bedc4b2a2ec87d6e5b5d14a8605
Pavel Holy <gyro1125@gmail.com>
2025-08-25 01:19:44 +0200

    Nvim: Add bind for formatted debug print for rust

--- home/nvim/after/ftplugin/rust.lua
+++ home/nvim/after/ftplugin/rust.lua
@@ -5,4 +5,5 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.keymap.set({"n", "x"}, "<leader>p", [[:s/\v^( *)(.*)/\1eprintln!("\2 = {:?}", \2);<CR>:noh<CR>]], { buffer = true } );
+vim.keymap.set({"n", "x"}, "<leader>p", [[:s/\v^( *)(.*)/\1eprintln!("\2 = {:?}", (\2));<CR>:noh<CR>]], { buffer = true } );
+vim.keymap.set({"n", "x"}, "<leader>P", [[:s/\v^( *)(.*)/\1eprintln!("\2 = {:#?}", (\2));<CR>:noh<CR>]], { buffer = true } );
