63a0df2e1de33ae87215a885f0963c583115187d
Pavel Holy <gyro1125@gmail.com>
2025-10-08 18:40:58 +0200

    Nvim: Format golang files on save

--- home/nvim/plugin-config/conform.lua
+++ home/nvim/plugin-config/conform.lua
@@ -3,6 +3,7 @@ require'conform'.setup{
         python = { "ruff_format", "ruff_organize_imports" },
         c = { "clang-format" },
         cpp = { "clang-format" },
+        go = { "gofmt" },
     },
     format_on_save = {
         timeout_ms = 2000,
