59a514681eaed4f286f83ecb67adb47965f35603
Pavel Holy <gyro1125@gmail.com>
2026-02-13 23:27:16 +0100

    Ags: Make eyetimer more visible

--- home/ags/src/eyetimer.js
+++ home/ags/src/eyetimer.js
@@ -1,12 +1,36 @@
 import { Item, Icon, Text, Progression, ClassManager, TooltipManager, Box } from './utils.js'
 
 const minutes = 20
+const pulsating_period_s = 4
 const v = Variable(0)
 
 var interval_handle = null
+var pulsating_handle = null
 var state // idle, running, completed
 const color_manager = new ClassManager([], ['yellow', 'red'])
+const pulsating_manager = new ClassManager([], ['pulsating-a', 'pulsating-b'])
 start()
+
+function start_pulsating() {
+  const pulsating_func = () => {
+    pulsating_manager.set('pulsating-a')
+    setTimeout(() => {
+      if(pulsating_handle !== null) {
+        pulsating_manager.set('pulsating-b')
+      }
+    }, pulsating_period_s * 1000 / 2)
+  }
+  pulsating_func()
+  pulsating_handle = setInterval(pulsating_func, pulsating_period_s * 1000)
+}
+
+function stop_pulsating() {
+  if(pulsating_handle !== null) {
+    clearInterval(pulsating_handle)
+    pulsating_handle = null
+    pulsating_manager.reset()
+  }
+}
 
 function start() {
   state = 'running'
@@ -14,6 +38,7 @@ function start() {
   // tooltip_manager.set(`${minutes - v.value} min`)
   // icon.label = '󱎫',
   color_manager.reset()
+  stop_pulsating()
   interval_handle = setInterval(() => {
     v.value++
     // tooltip_manager.set(`${minutes - v.value} min`)
@@ -32,6 +57,7 @@ function cancel() {
   color_manager.set('yellow')
   if(interval_handle !== null)
     clearInterval(interval_handle)
+  stop_pulsating()
 }
 
 function completed() {
@@ -39,6 +65,7 @@ function completed() {
   // tooltip_manager.set('Completed')
   // icon.label = '󱫌'
   color_manager.set('red')
+  start_pulsating()
 }
 
 export function Eyetimer(bar) {
@@ -62,7 +89,7 @@ export function Eyetimer(bar) {
 
   // const tooltip_manager = new TooltipManager()
   bar.add_managed_item(color_manager, item)
-
+  bar.add_managed_item(pulsating_manager, item)
 
   return Widget.EventBox({
     child: item,
