e5c92bc7146a34c6d1717892097ca97b8d7de063
Pavel Holy <gyro1125@gmail.com>
2026-04-15 13:21:08 +0200

    Ranger: Set preview file size limit

--- home/ranger.nix
+++ home/ranger.nix
@@ -4,6 +4,7 @@
     enable = true;
     extraConfig = ''
       set show_hidden true
+      set preview_max_size 1048576
       map <CR> move right=1
       map l eval fm.move(right=1) if fm.thisfile.is_directory else fm.edit_file(fm.thisfile.path)
     '';
