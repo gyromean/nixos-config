b3c59b9d39e5985fd9b9ce351e1167f2d4678779
Pavel Holy <gyro1125@gmail.com>
2026-07-01 13:08:18 +0200

    Nvim: Make lazygit persistent when opening a new file from it, and enable certain config options for snacks

--- home/nvim/plugin-config/snacks.lua
+++ home/nvim/plugin-config/snacks.lua
@@ -1,3 +1,14 @@
 local snacks = require("snacks")
 
+snacks.config.input.enabled = true
+snacks.config.picker.enabled = true
+snacks.config.terminal.enabled = true
+snacks.config.lazygit.config = {
+    os = {
+        edit = [[ [ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-tab {{filename}}) ]],
+        editAtLine = [[ [ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-tab {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>") ]],
+        openDirInEditor = [[ [ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-tab {{dir}}) ]],
+    },
+}
+
 vim.keymap.set("n", "<leader>g", snacks.lazygit.open, { desc = "Open lazygit" })
