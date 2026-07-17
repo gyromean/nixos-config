35b7582a88b148c305e2103219a5dd3b4fe2eb8d
Pavel Holy <gyro1125@gmail.com>
2026-07-17 17:57:22 +0200

    Bump opencode to 1.17.20

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     pala.url = "github:gyromean/pala"; # must NOT follow nixpkgs, use its own locked nixpkgs
     hywoma.url = "github:gyromean/hywoma";
     opencode = {
-      url = "github:anomalyco/opencode?ref=v1.17.7";
+      url = "github:anomalyco/opencode?ref=v1.17.20";
     };
     handy.url = "github:cjpais/Handy";
   };
